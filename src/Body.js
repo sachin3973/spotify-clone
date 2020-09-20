41d3260d15e177d6ad77a4a3fcaed108dd14fe8a
Sachin Sharma <vsharma3973@gmail.com>
2020-09-20 22:06:56 +0530

    Now app is pulling user daily discover weekly playlist

--- src/Body.js
+++ src/Body.js
@@ -2,6 +2,10 @@ import React from "react";
 import "./Body.css";
 import { useDataLayerValue } from "./DataLayer";
 import Header from "./Header";
+import SongRow from "./SongRow";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import FavoriteIcon from "@material-ui/icons/Favorite";
 
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = useDataLayerValue();
@@ -11,13 +15,26 @@ function Body({ spotify }) {
       <Header spotify={spotify} />
 
       <div className="body__info">
-        <img src={discover_weekly?.images[0].url} alt="Discover Weekly" />
+        <img src={discover_weekly?.images[0].url} alt="" />
 
         <div className="body__infoText">
           <strong>PLAYLIST</strong>
           <h2>Discover Weekly</h2>
           <p>{discover_weekly?.description}</p>
         </div>
+      </div>
+
+      <div className="body__songs">
+        <div className="body__icons">
+          <PlayCircleFilledIcon className="body__shuffle" />
+          <FavoriteIcon fontSize="large" />
+          <MoreHorizIcon />
+        </div>
+        {/* List of songs */}
+
+        {discover_weekly?.tracks.items.map((item) => (
+          <SongRow track={item.track} />
+        ))}
       </div>
     </div>
   );
